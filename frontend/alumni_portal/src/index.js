2f362dc3ebe1bc9d1edec98831c002b3c2cb86a3
Vishal Pal <118637617+Huge1729@users.noreply.github.com>
2024-08-21 00:15:34 +0530

    Update index.js

--- frontend/alumni_portal/src/index.js
+++ frontend/alumni_portal/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+
+// Creating the reactdom by considering the root id 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <BrowserRouter>
